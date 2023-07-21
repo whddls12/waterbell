ecf639c2374070c4d4d732c08beadfbdbcc5af14
이효경 <gyrud0482@naver.com>
2023-07-21 15:41:16 +0900

    project setting

---
+++ backend/fcc/src/test/java/com/ssafy/fcc/FccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.fcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
