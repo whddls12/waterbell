7e24c0f72114f73590d29406458b6ce9cbac9d53
jjunehee <jjhjjh1159@gmail.com>
2023-07-26 13:12:38 +0900

    수정

--- backend/fcc/src/main/java/com/ssafy/fcc/domain/log/ControlLog.java
+++ backend/fcc/src/main/java/com/ssafy/fcc/domain/log/ControlLog.java
@@ -1,35 +1,35 @@
 package com.ssafy.fcc.domain.log;
 
+
 import com.ssafy.fcc.domain.facility.Facility;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 
 @Entity
-@Getter
-@Setter
-public class ControlLog {
-
+public class controlLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "control_id")
-    private BigInteger id;
+    private BigInteger controlId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "facility_id")
     private Facility facility;
 
+
     @Enumerated(EnumType.STRING)
-    private String category;
+    private Control category;
 
-    @Column(name = "control_id")
+    @Column(name = "control_time")
     private LocalDateTime controlTime;
-
     @Column(name = "water_height")
     private Integer waterHeight;
 
+
     private String command;
+
+
+
 }
