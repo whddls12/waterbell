5fba28fc02410f69f8e80a58e86f3f02ce68ea08
yunyeong <y6357@naver.com>
2023-07-27 11:56:11 +0900

    component 구조 생성

---
+++ frontend/waterbell/src/underroad/views/roadDashboardView.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <!-- <Map/> -->
+    </div>
+</template>
+<script>
+// import Map from '@/underroad/components/dashboard/roadDashNearUnderroad.vue'
+
+export default {
+  name: 'Dash'
+
+}</script>
+<style lang="">
+
+</style>
