fc9f572d3360c48642fac286b4f7b73de047d2ac
yunyeong <y6357@naver.com>
2023-07-31 12:59:24 +0900

    merge 과정

--- frontend/waterbell/src/underroad/components/dashboard/roadDashNearUnderroad.vue
+++ frontend/waterbell/src/underroad/components/dashboard/roadDashNearUnderroad.vue
@@ -6,45 +6,45 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from "vue";
+import { defineComponent, onMounted, ref } from 'vue'
 
 export default defineComponent({
-  name: "roadDashMapVue",
+  name: 'roadDashMapVue',
   setup() {
-    const map = ref<any>(null);
+    const map = ref<any>(null)
 
     const initMap = () => {
-      const container = document.getElementById("map");
+      const container = document.getElementById('map')
       const options = {
         center: new (window as any).kakao.maps.LatLng(
           36.3549114724545,
           127.345907414374
         ),
-        level: 8,
-      };
-      map.value = new (window as any).kakao.maps.Map(container, options);
-    };
+        level: 8
+      }
+      map.value = new (window as any).kakao.maps.Map(container, options)
+    }
 
     const loadScript = () => {
-      const script = document.createElement("script");
-      script.type = "text/javascript";
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
       script.src =
-        "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=[key]&libraries=clusterer,drawing,services";
-      script.addEventListener("load", () => {
-        (window as any).kakao.maps.load(initMap);
-      });
-      document.head.appendChild(script);
-    };
+        '//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=[key]&libraries=clusterer,drawing,services'
+      script.addEventListener('load', () => {
+        ;(window as any).kakao.maps.load(initMap)
+      })
+      document.head.appendChild(script)
+    }
 
     onMounted(() => {
       if ((window as any).kakao && (window as any).kakao.maps) {
-        initMap();
+        initMap()
       } else {
-        loadScript();
+        loadScript()
       }
-    });
-  },
-});
+    })
+  }
+})
 </script>
 
 <style lang="css">
