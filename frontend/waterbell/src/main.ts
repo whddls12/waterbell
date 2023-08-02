a985f5cd6a22696b27697a210f977fbd0f431a7d
yunyeong <y6357@naver.com>
2023-08-02 16:59:08 +0900

    merge 정리

--- frontend/waterbell/src/main.ts
+++ frontend/waterbell/src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
-import Geolocation from 'vue-geolocation-api'
+// import Geolocation from 'vue-geolocation-api'
 
 axios.defaults.baseURL = 'http://localhost:8080'
 
