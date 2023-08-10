6f693e50a1f4beab1e5fac8199f2a99b347a0ec4
yunyeong <y6357@naver.com>
2023-08-10 09:58:56 +0900

    Feat: 네이버 소셜(수정중)

--- frontend/waterbell/src/undergroundParkingLot/views/member/naverSocialRedirect.vue
+++ frontend/waterbell/src/undergroundParkingLot/views/member/naverSocialRedirect.vue
@@ -4,35 +4,63 @@
 
 <script>
 import { defineComponent, onCreated } from 'vue'
-import apiModule from '@/types/apiClient'
-import router from 'vue-router'
+
+import { useRouter, useRoute } from 'vue-router'
+import http from '@/types/http'
 export default defineComponent({
   name: 'naverSocialRedirect',
   setup() {
-    const api = apiModule.api
-    console.log('이건/')
-    onCreated(() => {
-      function create() {
-        const uri = router.parseQuery
-        console.log(uri)
-        console.log('이거 실행?')
-        const urlParams = new URLSearchParams(window.location.search)
-        const code = urlParams.get('code')
-        const state = urlParams.get('state')
+    const router = useRouter()
+    const route = useRoute()
+    // const api = apiModule.api
+    // console.log('이건/')
+    // onCreated(() => {
+    //   function create() {
+    //     const uri = router.parseQuery
+    //     console.log(uri)
+    //     console.log('이거 실행?')
+    //     const urlParams = new URLSearchParams(window.location.search)
+    //     const code = urlParams.get('code')
+    //     const state = urlParams.get('state')
 
-        if (code && state) {
-          // 서버에 인증 코드와 상태를 보내 액세스 토큰을 얻는다.
-          api
-            .post(`/login/oauth2/code/naver`, { code, state })
-            .then((res) => {
-              console.log(res.data)
-              // 성공적으로 토큰을 받았다면, 이 토큰을 사용하여 사용자 정보를 얻거나 다른 작업을 수행할 수 있다.
-              // 필요에 따라 다른 페이지로 리다이렉트할 수도 있다.
-            })
-            .catch((err) => console.error(err))
-        }
+    //     if (code && state) {
+    //       // 서버에 인증 코드와 상태를 보내 액세스 토큰을 얻는다.
+    //       api
+    //         .post(`/login/oauth2/code/naver`, { code, state })
+    //         .then((res) => {
+    //           console.log(res.data)
+    //           // 성공적으로 토큰을 받았다면, 이 토큰을 사용하여 사용자 정보를 얻거나 다른 작업을 수행할 수 있다.
+    //           // 필요에 따라 다른 페이지로 리다이렉트할 수도 있다.
+    //         })
+    //         .catch((err) => console.error(err))
+    //     }
+    //   }
+    // })
+
+    const nvalidateMember = async (code, state) => {
+      const response = await http.post(
+        `/oauth2/naver?code=${code}&state=${state}`,
+        {},
+        { withCredentials: true }
+      )
+
+      console.log(response.data)
+      //  = { ...response.data }
+    }
+    const invalidateMember = async () => {
+      alert('로그인 실패')
+    }
+
+    const nLogin = async (code, state) => {
+      try {
+        await nvalidateMember(code, state)
+        alert('네이버 로그인 성공')
+        router.push({ name: 'Home' })
+      } catch (err) {
+        alert('네이버 로그인 실패')
+        console.error(err)
       }
-    })
+    }
     // create()
   }
 })
--- frontend/waterbell/src/undergroundParkingLot/views/member/parkLoginView.vue
+++ frontend/waterbell/src/undergroundParkingLot/views/member/parkLoginView.vue
@@ -33,7 +33,7 @@ div
             <div class="socialBox">
               <img src="@/assets/Login&Signup/OR.png" />
               <div>
-                <button class="imgBtn" type="button" @click="handleNaverLogin">
+                <button class="imgBtn" type="button" @click="naverLogin">
                   <img
                     src="@/assets/Login&Signup/naver_login.png"
                     class="socialLoginBtn"
@@ -41,11 +41,10 @@ div
                 </button>
               </div>
               <div>
-                <button class="imgBtn" type="button">
+                <button class="imgBtn" type="button" @click="kakaoLogin">
                   <img
                     src="@/assets/Login&Signup/kakao_login.png"
                     class="socialLoginBtn"
-                    @click="kakaoLogin"
                   />
                 </button>
               </div>
@@ -53,7 +52,7 @@ div
           </form>
         </div>
 
-        <a href="#none">비밀번호를 잊어버리셨나요?</a>
+        <a href="#none">비밀번호 찾기?</a>
       </div>
     </div>
   </div>
@@ -61,10 +60,8 @@ div
 <script>
 import { defineComponent, ref } from 'vue'
 import { useStore } from 'vuex'
-
-// import apiModule from '@/types/apiClient'
-// import router from '@/router/index'
-import axios from 'axios'
+import { useRouter, useRoute } from 'vue-router'
+import http from '@/types/http'
 export default defineComponent({
   name: 'parkLogin',
   setup() {
@@ -72,31 +69,84 @@ export default defineComponent({
     const id = ref('')
     const password = ref('')
     const store = useStore() // 전역 스토어를 가져옵니다.
+    const router = useRouter()
+    const route = useRoute()
+
     const login = () => {
       store.dispatch('auth/memberLogin', {
         loginId: id.value,
         password: password.value
       })
     }
+    const naverLogin = () => {
+      //로컬 서버 연결용
+      const url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=dIwg6T0yWa9t8y2yMsHJ&redirect_uri=http://localhost:8081/auth/naver&state=WaterBell`
 
-    const uri = '/login/oauth2/code'
-    const clinet_id = 'dIwg6T0yWa9t8y2yMsHJ'
+      //운영용
+      // const url= `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=dIwg6T0yWa9t8y2yMsHJ&redirect_uri=http://i9b101.p.ssafy.io:8080/login/oauth2/code/naver&state=WaterBell`
+      console.log('가고 있냐 ')
+      window.location.href = url
+    }
 
-    const handleNaverLogin = async () => {
-      const clientId = 'dIwg6T0yWa9t8y2yMsHJ'
-      const redirectURI = encodeURIComponent('http://localhost:8081/auth/naver')
-      const state = 'WaterBell'
+    const nvalidateMember = async (code, state) => {
+      // const response = await http.post(
+      //   `/oauth2/naver?code=${code}&state=${state}`,
+      //   {},
+      //   { withCredentials: true }
+      // )
 
-      const authURL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectURI}&state=${state}`
+      http
+        .get('/login/oauth2/code/naver', {
+          params: {
+            code: this.$route.query.code,
+            state: this.$route.query.state
+          }
+        })
+        .then((response) => {
+          if (response.data.message === 'success') {
+            if (response.data.type === 'join') {
+              router.push('/park/join')
+              // 회원 가입 페이지로 리다이렉션하거나 알림 표시
+            } else {
+              // 로그인 처리
+              router.push('/')
+            }
+          } else {
+            console.error(response.data.exception) // 실패 시 서버에서 보낸 예외 메시지 출력
+          }
+        })
 
-      window.location.href = authURL
+      // console.log(response.data)
+      //  = { ...response.data }
+    }
+    const invalidateMember = async () => {
+      alert('로그인 실패')
+    }
+
+    const nLogin = async (code, state) => {
+      try {
+        await nvalidateMember(code, state)
+        alert('네이버 로그인 성공')
+        router.push({ name: 'Home' })
+      } catch (err) {
+        alert('네이버 로그인 실패')
+        console.error(err)
+      }
+    }
+
+    // 쿼리스트링으로 code를 받게되면 이를 통해 서버에 요청을 보내는 로직을 수행한다.
+    if (route.query.code && route.query.state) {
+      nLogin(route.query.code, route.query.state)
     }
 
     return {
       id,
       password,
       login, // login 함수를 템플릿에서 사용할 수 있도록 반환합니다.
-      handleNaverLogin
+      naverLogin,
+      nvalidateMember,
+      nLogin,
+      invalidateMember
     }
   }
 })
