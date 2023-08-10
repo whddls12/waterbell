10b49a46eb707a593736b2f4a06f10bfa1f1e099
jjunehee <jjhjjh1159@gmail.com>
2023-08-10 13:16:54 +0900

    Feat: Hub 추가

--- iot/RASPBERRYPI/Hub.py
+++ iot/RASPBERRYPI/Hub.py
@@ -3,10 +3,11 @@ import paho.mqtt.client as mqtt
 import threading
 
 THING_NAME = 'IoT'
-CERTPATH =  "" # cert파일 경로
-KEYPATH = "" # key 파일 경로
-CAROOTPATH = "" # RootCaPem 파일 경로
-
+ENDPOINT = "a221zxhtj4qlos-ats.iot.us-east-2.amazonaws.com"
+CERTPATH =  "/home/jjhjjh/Desktop/Hub/IoT.cert.pem" # cert파일 경로
+KEYPATH = "/home/jjhjjh/Desktop/Hub/IoT.private.key" # key 파일 경로
+CAROOTPATH = "/home/jjhjjh/Desktop/Hub/root-CA.crt" # RootCaPem 파일 경로
+FACILITY = "10/"
 msg_count = 0
 
 def on_connect(client, userdata, flags, rc):
@@ -24,7 +25,7 @@ def connect_mqtt1():
 def connect_mqtt2():
     IoTCore = mqtt.Client()
     IoTCore.tls_set(CAROOTPATH, certfile= CERTPATH, keyfile=KEYPATH, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
-    IoTCore.connect("a221zxhtj4qlos-ats.iot.us-east-2.amazonaws.com", 8883, 60)
+    IoTCore.connect(ENDPOINT, 8883, 60)
     IoTCore.on_connect = on_connect
     return IoTCore
 
@@ -33,22 +34,21 @@ def subscribe1(arduino, IoTCore):
     arduino.on_message = lambda client, userdata, msg: on_message1(arduino, userdata, msg, IoTCore)
 
 def subscribe2(IoTCore, arduino):
-   IoTCore.subscribe("Server/10/BOARD")
-   IoTCore.on_message = lambda client, userdata, msg: on_message2(IoTCore, userdata, msg, arduino)
+    IoTCore.subscribe("Server/10/BOARD")
+    IoTCore.on_message = lambda client, userdata, msg: on_message2(IoTCore, userdata, msg, arduino)
 
 def on_message1(client, userdata, msg, IoTCore):
     global msg_count
     payload = msg.payload.decode()
     print(f"Received message: {payload}")
 
-    result = IoTCore.publish("Arduino/SENSOR", payload, qos=1)
+    result = IoTCore.publish("Arduino/SENSOR", FACILITY+payload, qos=1)
     status = result.rc
     if status == mqtt.MQTT_ERR_SUCCESS:
         print(f"Send `{payload}` to topic `Arduino/SENSOR`")
     else:
         print(f"Failed to send message to topic `Arduino/SENSOR`")
 
-    msg_count += 1
 
 def on_message2(client, userdata, msg, arduino):
     global msg_count
@@ -59,8 +59,6 @@ def on_message2(client, userdata, msg, arduino):
     status = result.rc
     if status == mqtt.MQTT_ERR_SUCCESS:
         print(f"Send `{payload}` to topic `Server/PLATE`")
-
-    msg_count += 1
 
 def run():
     arduino = connect_mqtt1()
