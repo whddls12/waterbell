64e0729250d32c3b95092da2c38d90220f5cb427
이효경 <gyrud0482@naver.com>
2023-08-15 14:25:38 +0900

    Fix : 신고접수 화면css 추가

--- frontend/waterbell/src/views/Home.vue
+++ frontend/waterbell/src/views/Home.vue
@@ -210,7 +210,7 @@ export default defineComponent({
   margin-top: 50px;
 
   display: flex;
-  width: 1000px;
+  width: 1150px;
   padding: 80px 0px;
   flex-direction: column;
   align-items: center;
