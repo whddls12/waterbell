64210b8f8f61dcb4171b83cf9da7cdd54506a891
kky156 <kky156@naver.com>
2023-08-16 23:26:07 +0900

    Feat : servo motor

--- iot/Underground/actuator.py
+++ iot/Underground/actuator.py
@@ -3,13 +3,38 @@ import paho.mqtt.client as mqtt
 import threading
 import RPi_I2C_driver
 import RPi.GPIO as GPIO
+
 mylcd = RPi_I2C_driver.lcd()
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(16,GPIO.OUT)
 GPIO.setup(20,GPIO.OUT)
 GPIO.setup(21,GPIO.OUT)
+
+servo_pin = 18
+GPIO.setup(servo_pin, GPIO.OUT)
+GPIO.output(servo_pin, False)
+pwm = GPIO.PWM(servo_pin, 50)
+
+# 0 : OFF 1: ON
+servoStatus = 0
+
+sub_servo_topic = "Server/BOARD"
+sub_lcd_topic = "Server/STATUS"
+
+mqtt_server = "172.30.1.38"
+facility_id = "10"
+
+pub_topic = "Arduino/CONTROL"
+
+
+def publish_status(client, topic, interval):
+    global servoStatus
+    while True:
+        client.publish(topic, servoStatus, qos = 0)
+        time.sleep(interval)
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -24,10 +49,33 @@ def connect_mqtt():
     return arduino
 
 def subscribe(arduino):
-    arduino.subscribe("Server/STATUS")
-    arduino.on_message = on_message
+    arduino.subscribe(sub_servo_topic)
+    arduino.message_callback_add(sub_servo_topic, on_message_servo)
 
-def on_message(client, userdata, msg):
+
+    arduino.subscribe(sub_lcd_topic)
+    arduino.message_callback_add(sub_lcd_topic, on_message_lcd)
+
+def on_message_servo(client, userdata, msg):
+    global servoStatus
+    global pwm
+    payload = msg.payload.decode()
+    
+    pwm.start(3.0)
+    timeA = 1.0
+
+    if(payload == "ON"):
+        print("ON")
+        pwm.ChangeDutyCycle(7.5)
+
+    else:
+        print("OFF")
+        pwm.ChangeDutyCycle(3.0)
+    time.sleep(timeA)
+
+
+
+def on_message_lcd(client, userdata, msg):
     payload = msg.payload.decode()
     print(f"Received message: {payload}")
 
@@ -55,12 +103,20 @@ def on_message(client, userdata, msg):
         GPIO.output(21,False)
         GPIO.output(20,True)
 
+def publisher(client, topic, message):
+    QOS = 0
+    client.publish(topic, message, QOS)
+
+
+
 def run():
     arduino = connect_mqtt()
 
     subscribe(arduino)
 
     arduino_thread = threading.Thread(target=arduino.loop_forever)
+    pub_thread = threading.Thread(target = publish_status, args=(arduino, pub_topic, 5))
+
 
     arduino_thread.start()
 
