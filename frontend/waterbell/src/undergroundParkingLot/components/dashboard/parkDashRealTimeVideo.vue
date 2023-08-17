512ad69f69d136da42fa97d6452ce9e366a6f6b9
황종인 <ghkdwhddls12@daum.net>
2023-08-17 22:45:20 +0900

    fix: 현황판 cctv 화면 소제목 수정

--- frontend/waterbell/src/undergroundParkingLot/components/dashboard/parkDashRealTimeVideo.vue
+++ frontend/waterbell/src/undergroundParkingLot/components/dashboard/parkDashRealTimeVideo.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="dash-box-content">
         <div class="dash-box-content" id="cctv-box-content">
-          <p class="text-start">지하차도 외부</p>
+          <p class="text-start">지하차도 내부</p>
           <div class="cctvBox">
             <div>
               <img id="cctv1" />
